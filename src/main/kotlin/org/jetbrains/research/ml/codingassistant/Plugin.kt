2f1196ad10f18bce639bd3fc8b5abe56a4a88c8e
Anastasiia.Birillo <nbirillo@mail.ru>
2020-10-07 13:14:05 +0300

    Init empty plugin

---
+++ src/main/kotlin/org/jetbrains/research/ml/codingassistant/Plugin.kt
@@ -0,0 +1,9 @@
+package org.jetbrains.research.ml.codingassistant
+
+import com.intellij.openapi.diagnostic.Logger
+
+object Plugin {
+    private val logger: Logger = Logger.getInstance(javaClass)
+
+    const val PLUGIN_NAME = "Coding Assistant"
+}
