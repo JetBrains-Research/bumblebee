28f3a949a0f1e29d9b343fe0715ece2e1cf8cad5
Anastasiia.Birillo <nbirillo@mail.ru>
2020-10-13 17:06:08 +0300

    Cleanup dependencies: use JUnit from IntelliJ SDK

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,10 +17,6 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
-
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.2")
-    testImplementation("org.junit.jupiter:junit-jupiter-params:5.0.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.2")
 }
 
 intellij {
