655666dd12bba91f7350ac166718a277aba5af32
Artem Bobrov <artembobrov3059@gmail.com>
2020-10-28 08:13:06 +0300

    [transformation][mta]: fix test

--- src/main/kotlin/org/jetbrains/research/ml/ast/transformations/PyUtils.kt
+++ src/main/kotlin/org/jetbrains/research/ml/ast/transformations/PyUtils.kt
@@ -3,17 +3,6 @@ package org.jetbrains.research.ml.ast.transformations
 import com.intellij.psi.PsiElement
 
 object PyUtils {
-    fun createPyIfElsePart(ifElsePart: PyIfPart): PyIfPart {
-        assert(ifElsePart.isElif)
-        val generator = PyElementGenerator.getInstance(ifElsePart.project)
-        val ifStatement = generator.createFromText(
-            LanguageLevel.PYTHON36,
-            PyIfStatement::class.java,
-            "if ${ifElsePart.condition?.text ?: ""}:\n\t${ifElsePart.statementList.text}"
-        )
-        return ifStatement.ifPart
-    }
-
     fun createAssignment(target: PsiElement, value: PsiElement): PyAssignmentStatement {
         val generator = PyElementGenerator.getInstance(target.project)
         return generator.createFromText(
