24c45dc1317a6c6e40d76e565603ecfe1f51318a
Artem Bobrov <artembobrov3059@gmail.com>
2020-11-10 20:49:09 +0300

    test fix

--- src/test/kotlin/org/jetbrains/research/ml/ast/transformations/util/TransformationsTest.kt
+++ src/test/kotlin/org/jetbrains/research/ml/ast/transformations/util/TransformationsTest.kt
@@ -7,6 +7,7 @@ package org.jetbrains.research.ml.ast.transformations.util
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.command.WriteCommandAction
 import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiFile
 import com.intellij.psi.codeStyle.CodeStyleManager
 import org.jetbrains.research.ml.ast.util.FileTestUtil
 import org.jetbrains.research.ml.ast.util.ParametrizedBaseTest
@@ -18,8 +19,7 @@ import kotlin.reflect.KFunction
 
 @Ignore
 open class TransformationsTest(testDataRoot: String) : ParametrizedBaseTest(testDataRoot) {
-
-    lateinit var codeStyleManager: CodeStyleManager
+    private lateinit var codeStyleManager: CodeStyleManager
 
     @JvmField
     @Parameterized.Parameter(0)
@@ -52,22 +52,25 @@ open class TransformationsTest(testDataRoot: String) : ParametrizedBaseTest(test
     ) {
         LOG.info("The current input file is: ${inFile.path}")
         LOG.info("The current output file is: ${outFile.path}")
-        val psiInFile = myFixture.configureByFile(inFile.name)
-        val expectedPsiInFile = myFixture.configureByFile(outFile.name)
-        WriteCommandAction.runWriteCommandAction(project) { // reformat the expected file
-            codeStyleManager.reformat(expectedPsiInFile)
-        }
+        val psiInFile = getPsiFile(inFile.name)
+        val expectedPsiInFile = getPsiFile(outFile.name)
         val expectedSrc = expectedPsiInFile.text
         LOG.info("The expected code is:\n$expectedSrc")
         ApplicationManager.getApplication().invokeAndWait {
             transformation(psiInFile, true)
         }
-
-        WriteCommandAction.runWriteCommandAction(project) { // reformat the transformed file
-            codeStyleManager.reformat(psiInFile)
-        }
         val actualSrc = psiInFile.text
         LOG.info("The actual code is:\n$actualSrc")
         assertEquals(expectedSrc, actualSrc)
+    }
+
+    private fun getPsiFile(filename: String, toReformatFile: Boolean = true): PsiFile {
+        val psiFile = myFixture.configureByFile(filename)
+        if (toReformatFile) {
+            WriteCommandAction.runWriteCommandAction(project) { // reformat the expected file
+                codeStyleManager.reformat(psiFile)
+            }
+        }
+        return psiFile
     }
 }
