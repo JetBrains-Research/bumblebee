f15954d512434f5befd805b23f215393ea9d61d3
Artem Bobrov <artembobrov3059@gmail.com>
2020-11-10 22:57:36 +0300

    add test. now working

--- src/test/resources/org/jetbrains/research/ml/ast/transformations/multipleTargetAssignment/data/in_3.py
+++ src/test/resources/org/jetbrains/research/ml/ast/transformations/multipleTargetAssignment/data/in_3.py
@@ -2,6 +2,7 @@ def main():
     class A:
         pass
     x = y = z = A()
+    i = j = 1
     a = 1
     b = "hello"
     c = -2.0
--- src/test/resources/org/jetbrains/research/ml/ast/transformations/multipleTargetAssignment/data/out_3.py
+++ src/test/resources/org/jetbrains/research/ml/ast/transformations/multipleTargetAssignment/data/out_3.py
@@ -4,6 +4,8 @@ def main():
     x = A()
     y = x
     z = y
+    i = 1
+    j = i
     a = 1
     b = "hello"
     c = -2.0
