8a46842485c8b3c1eca181b0d642df7105346e18
Anastasiia.Birillo <nbirillo@mail.ru>
2020-11-17 15:41:24 +0300

    Add base class

---
+++ src/main/kotlin/org/jetbrains/research/ml/ast/transformations/commentsRemoval/CommentsRemovalTransformation.kt
@@ -0,0 +1,19 @@
+package org.jetbrains.research.ml.ast.transformations.commentsRemoval
+
+import com.intellij.psi.PsiElement
+import org.jetbrains.research.ml.ast.transformations.Transformation
+
+class CommentsRemovalTransformation : Transformation {
+    override val metadataKey: String
+        get() = TODO("Not yet implemented")
+
+    override fun inverseApply(psiTree: PsiElement) {
+        TODO("Not yet implemented")
+    }
+
+    override fun apply(psiTree: PsiElement, toStoreMetadata: Boolean) {
+//        val augStatements = PsiTreeUtil.collectElementsOfType(psiTree, PyAugAssignmentStatement::class.java)
+
+        TODO("Not yet implemented")
+    }
+}
