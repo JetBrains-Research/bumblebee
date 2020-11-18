e73302dbf2803888390683a1d03b3049ed52f109
Anastasiia.Birillo <nbirillo@mail.ru>
2020-11-18 11:10:20 +0300

    Fix a bug with triple quotes assigment case

--- src/main/kotlin/org/jetbrains/research/ml/ast/transformations/commentsRemoval/CommentsRemovalVisitor.kt
+++ src/main/kotlin/org/jetbrains/research/ml/ast/transformations/commentsRemoval/CommentsRemovalVisitor.kt
@@ -1,8 +1,11 @@
 package org.jetbrains.research.ml.ast.transformations.commentsRemoval
 
 import com.intellij.psi.PsiComment
+import com.intellij.psi.util.elementType
+import com.intellij.psi.util.parents
 import com.jetbrains.python.PyTokenTypes
 import com.jetbrains.python.psi.PyElementVisitor
+import com.jetbrains.python.psi.PyExpressionStatement
 import com.jetbrains.python.psi.PyStringLiteralExpression
 
 class CommentsRemovalVisitor : PyElementVisitor() {
@@ -23,4 +26,5 @@ class CommentsRemovalVisitor : PyElementVisitor() {
 
     private val PyStringLiteralExpression.isTripleQuotedString: Boolean
         get() = this.stringNodes.size == 1 && stringNodes[0].elementType === PyTokenTypes.TRIPLE_QUOTED_STRING
+            && this.parents.toList().isNotEmpty() && this.parents.first() is PyExpressionStatement
 }
