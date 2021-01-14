cef0457182cc181f1996a5b728377da042db321e
Anastasiia.Birillo <nbirillo@mail.ru>
2021-01-14 07:11:36 +0300

    Bug fix

--- src/main/kotlin/org/jetbrains/research/ml/ast/gumtree/diff/PsiElementTransformer.kt
+++ src/main/kotlin/org/jetbrains/research/ml/ast/gumtree/diff/PsiElementTransformer.kt
@@ -1,17 +1,29 @@
 package org.jetbrains.research.ml.ast.gumtree.diff
 
 import com.github.gumtreediff.actions.model.*
+import com.github.gumtreediff.tree.ITree
+import com.intellij.codeInsight.editorActions.smartEnter.SmartEnterProcessor
 import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.editor.EditorFactory
+import com.intellij.openapi.fileEditor.FileDocumentManager
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiManager
 import com.intellij.util.IncorrectOperationException
 import com.jetbrains.python.psi.LanguageLevel
 import com.jetbrains.python.psi.PyElementGenerator
+import org.jetbrains.research.ml.ast.gumtree.psi.label
 import org.jetbrains.research.ml.ast.gumtree.tree.Numbering
+import org.jetbrains.research.ml.ast.gumtree.tree.Numbering.PsiTreeUtils.Companion.id
+import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.collections.HashSet
 
 class PsiElementTransformer(
     project: Project,
-    srcPsi: PsiElement,
+    val srcPsi: PsiElement,
     dstPsi: PsiElement,
     private val numbering: Numbering
 ) {
@@ -19,6 +31,15 @@ class PsiElementTransformer(
 
     private val srcPsiNodes = getPsiNodes(srcPsi)
     private val dstPsiNodes = getPsiNodes(dstPsi)
+//
+//    private lateinit var srcEditor: Editor
+//    private lateinit var srcPsiFile: VirtualFile
+//
+//    init {
+//        ApplicationManager.getApplication().invokeAndWait {
+//            srcEditor = getEditor(srcPsi)
+//        }
+//    }
 
     private fun getPsiNodes(psi: PsiElement): List<PsiElement> {
         return ApplicationManager.getApplication().runReadAction<List<PsiElement>> {
@@ -27,27 +48,61 @@ class PsiElementTransformer(
     }
 
     // Should be run in WriteAction
-    fun applyAction(action: Action) {
+    private fun applyAction(action: Action, insertedNode: MutableSet<Int>) {
         // TODO: should we store actions to compare with transformations actions??
         when (action) {
             is Delete -> action.applyDelete()
-            is Insert -> action.apply()
+            is Insert -> action.apply(insertedNode)
             is Update -> action.apply()
-            is Move -> action.apply()
+            is Move -> action.apply(insertedNode)
             else -> error("Unknown GumTree action ${action.name}")
         }
     }
 
+//    private fun getEditor(psiElement: PsiElement): Editor {
+//        srcPsiFile = psiElement.containingFile.virtualFile
+//        return ApplicationManager.getApplication().runReadAction<Editor> {
+//            val doc = FileDocumentManager.getInstance().getDocument(srcPsiFile)!!
+//            EditorFactory.getInstance().createEditor(doc, project)!!
+//        }
+//    }
+
+    fun applyActions(actions: List<Action>) {
+        println("_____")
+        println(srcPsi.text)
+        println("_____")
+        val insertedNode: MutableSet<Int> = HashSet()
+        val actionsQueue: Queue<Action> = LinkedList(actions)
+        while (actionsQueue.isNotEmpty()) {
+            val action = actionsQueue.poll()
+//            applyAction(action)
+            try {
+                println("_____")
+                applyAction(action, insertedNode)
+                println(srcPsi.text)
+                println("_____")
+            } catch (e: IncorrectOperationException) {
+                println("Try execute action $action, but fail")
+                actionsQueue.add(action)
+            }
+        }
+//        EditorFactory.getInstance().releaseEditor(srcEditor)
+//        SmartEnterProcessor.commitDocument(srcEditor)
+//        PsiManager.getInstance(project).findFile(srcPsiFile)!!
+    }
+
     private fun Action.getPsiElementById(psiElements: List<PsiElement>, errorMessagePrefix: String = ""): PsiElement {
         val id = this.node.id
-        require(id <= psiElements.size) { "${errorMessagePrefix}PSI tree does not contain node with id $id" }
+        require(id < psiElements.size) { "${errorMessagePrefix}PSI tree does not contain node with id $id" }
         return psiElements[id]
     }
 
-    private fun executeInsert(element: PsiElement, parent: PsiElement, position: Int) {
+    private fun executeInsert(element: PsiElement, parent: PsiElement, position: Int,
+                              gumTreeNode: ITree, insertedNode: MutableSet<Int>) {
         if (!element.isValid) {
             return
         }
+        println("$position")
         val child = if (position < parent.children.size) {
             parent.children[position]
         } else {
@@ -56,10 +111,16 @@ class PsiElementTransformer(
         // We can invalidate PSI by applying GumTree transformations.
         // GumTree has <delete> actions for incorrect <insert> action, but later
         try {
-            parent.addAfter(element, child) as PsiElement
+            println(element.text)
+            println(parent.text)
+            parent.addAfter(element, child)
+            insertedNode.add(gumTreeNode.id)
+            // Filter according to postOder traversal
+            insertedNode.addAll(element.children.mapNotNull{ it.id }.filter { it >= gumTreeNode.id })
         } catch (e: IncorrectOperationException) {
             println("Try execute insert actions, but fail")
         }
+        println(insertedNode)
     }
 
     private fun Action.applyDelete() {
@@ -75,21 +136,29 @@ class PsiElementTransformer(
         psiNodes: List<PsiElement>,
         parentId: Int,
         position: Int,
-        errorMessagePrefix: String = ""
+        errorMessagePrefix: String = "",
+        insertedNode: MutableSet<Int>
     ) {
         val psiElement = this.getPsiElementById(psiNodes, errorMessagePrefix)
         // Don't insert a node which does exist
-        if (parentId > srcPsiNodes.size) {
+        if (parentId in insertedNode) {
             return
         }
-        val parentNode = srcPsiNodes[parentId]
-        executeInsert(psiElement, parentNode, position)
+        val parentNode = if (parentId > srcPsiNodes.size) {
+            dstPsiNodes[parentId]
+        } else {
+            srcPsiNodes[parentId]
+        }
+        println("ids ${node.id} $parentId")
+        println("labels ±${node.label}± ±${psiElement.label}± ±${parentNode.text}±")
+//        val parentNode = srcPsiNodes[parentId]
+        executeInsert(psiElement, parentNode, position, this.node, insertedNode)
     }
 
-    private fun Insert.apply() = this.applyInsert(dstPsiNodes, this.parent.id, this.position, "Destination ")
+    private fun Insert.apply(insertedNode: MutableSet<Int>) = this.applyInsert(dstPsiNodes, this.parent.id, this.position, "Destination ", insertedNode)
 
-    private fun Move.apply() {
-        this.applyInsert(srcPsiNodes, this.parent.id, this.position, "Source ")
+    private fun Move.apply(insertedNode: MutableSet<Int>) {
+        this.applyInsert(srcPsiNodes, this.parent.id, this.position, "Source ", insertedNode)
         this.applyDelete()
     }
 
--- src/test/kotlin/org/jetbrains/research/ml/ast/gumtree/diff/PsiElementTransformerTest.kt
+++ src/test/kotlin/org/jetbrains/research/ml/ast/gumtree/diff/PsiElementTransformerTest.kt
@@ -1,6 +1,8 @@
 package org.jetbrains.research.ml.ast.gumtree.diff
 
+import com.github.gumtreediff.io.TreeIoUtils
 import com.intellij.openapi.command.WriteCommandAction
+import com.intellij.psi.PsiFile
 import org.jetbrains.research.ml.ast.gumtree.Util
 import org.jetbrains.research.ml.ast.gumtree.tree.PostOrderNumbering
 import org.jetbrains.research.ml.ast.util.*
@@ -42,20 +44,28 @@ class PsiElementTransformerTest : ParametrizedBaseTest(getResourcesRootPath(::Ps
     //  a1 = False and False
     //  a2 = False or False
     @Test
-    fun `apply actions`() {
-        val srcPsi = myFixture.getPsiFile(srcFile!!)
-        val dstPsi = myFixture.getPsiFile(dstFile!!)
+    fun `apply src to dst actions`() = convertSrcToDst(srcFile!!, dstFile!!)
+
+    @Test
+    fun `apply dst to src actions`() = convertSrcToDst(dstFile!!, srcFile!!)
+
+    private fun convertSrcToDst(srcFile: File, dstFile: File) {
+        val srcPsi = myFixture.getPsiFile(srcFile)
+        val dstPsi = myFixture.getPsiFile(dstFile)
+        val expectedCode = dstPsi.text
         val srcContext = Util.getTreeContext(srcPsi, numbering)
         val dstContext = Util.getTreeContext(dstPsi, numbering)
         val matcher = Matcher(srcContext, dstContext)
         val actions = matcher.getEditActions()
+        val srcXml = TreeIoUtils.toXml(srcContext).toString().removeSuffix("\n")
+        val dstXml = TreeIoUtils.toXml(dstContext).toString().removeSuffix("\n")
         val w = PsiElementTransformer(project, srcPsi, dstPsi, numbering)
         WriteCommandAction.runWriteCommandAction(project) {
-            actions.forEachIndexed { i, a ->
-                println(i)
-                w.applyAction(a)
-            }
+            w.applyActions(actions)
+//            actions.forEach {
+//                w.applyAction(it)
+//            }
         }
-        assertEquals(dstPsi.text, srcPsi.text)
+        assertEquals(expectedCode, srcPsi.text)
     }
 }
