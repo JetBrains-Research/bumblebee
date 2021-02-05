e0d127d0595b8c0947edb04e667e34c7d8555e9e
Artem Bobrov <artembobrov3059@gmail.com>
2021-02-05 12:46:57 +0200

    refactor

--- src/test/kotlin/org/jetbrains/research/ml/ast/transformations/if_redundant_lines_removal/IfRedundantLinesRemovalTransformationTest.kt
+++ src/test/kotlin/org/jetbrains/research/ml/ast/transformations/if_redundant_lines_removal/IfRedundantLinesRemovalTransformationTest.kt
@@ -7,7 +7,9 @@ import org.junit.runner.RunWith
 import org.junit.runners.Parameterized
 
 @RunWith(Parameterized::class)
-class IfRedundantLinesRemovalTransformationTest : TransformationsWithSdkTest(getResourcesRootPath(::TransformationsTest)) {
+class IfRedundantLinesRemovalTransformationTest : TransformationsWithSdkTest(
+    getResourcesRootPath(::TransformationsTest)
+) {
     companion object {
         @JvmStatic
         @Parameterized.Parameters(name = "{index}: ({0}, {1})")
