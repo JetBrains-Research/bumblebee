ceb9c1849af5933c1420135c22905a26f0571a59
Nastya Birillo <anastasia.i.birillo@gmail.com>
2021-03-03 21:22:29 +0300

    Use Java 11

--- build.gradle.kts
+++ build.gradle.kts
@@ -45,13 +45,12 @@ ktlint {
 }
 
 tasks {
-    // Set the compatibility versions to 1.8
     withType<JavaCompile> {
-        sourceCompatibility = "1.8"
-        targetCompatibility = "1.8"
+        sourceCompatibility = "11"
+        targetCompatibility = "11"
     }
     withType<KotlinCompile> {
-        kotlinOptions.jvmTarget = "1.8"
+        kotlinOptions.jvmTarget = "11"
     }
 
     runIde {
