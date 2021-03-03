de61929e0b9e749a3f780bd2b9e999405f5b2473
Anastasiia.Birillo <nbirillo@mail.ru>
2021-03-03 20:38:15 +0300

    Reorganize gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,19 +37,7 @@ intellij {
     version = "2020.3.3"
     downloadSources = false
     setPlugins("PythonCore")
-}
-
-configure<JavaPluginConvention> {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-}
-
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
-}
-
-tasks.withType<JavaCompile> {
-    sourceCompatibility = "1.8"
-    targetCompatibility = "1.8"
+    updateSinceUntilBuild = true
 }
 
 ktlint {
@@ -57,6 +45,15 @@ ktlint {
 }
 
 tasks {
+    // Set the compatibility versions to 1.8
+    withType<JavaCompile> {
+        sourceCompatibility = "1.8"
+        targetCompatibility = "1.8"
+    }
+    withType<KotlinCompile> {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+
     runIde {
         val input: String? by project
         val output: String? by project
