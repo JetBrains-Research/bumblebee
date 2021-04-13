c0cb3ec2ca006e861bd0a61787d495366230a4f9
Artem Bobrov <artembobrov3059@gmail.com>
2021-04-13 12:47:09 +0300

    remove extra

--- ast-transformations-core/build.gradle.kts
+++
@@ -1,6 +0,0 @@
-dependencies {
-//    implementation(kotlin("stdlib-jdk8"))
-//
-//    implementation("com.github.gumtreediff", "core", "2.1.2")
-//    implementation("org.apache.commons:commons-lang3:3.12.0")
-}
