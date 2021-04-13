c6f0bd4464b9ec128eecde608e48f0f49de8c570
Artem Bobrov <artembobrov3059@gmail.com>
2021-04-13 12:32:55 +0300

    multi module

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,78 +2,138 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     java
-    kotlin("jvm") version "1.4.30"
+    val kotlinVersion = "1.4.30"
+    kotlin("jvm") version kotlinVersion
+    kotlin("plugin.serialization") version kotlinVersion
     id("org.jetbrains.intellij") version "0.7.2"
     id("com.github.johnrengelman.shadow") version "5.1.0"
     id("org.jetbrains.dokka") version "0.10.1"
     id("org.jlleitschuh.gradle.ktlint") version "9.4.1"
-    kotlin("plugin.serialization") version "1.4.20"
 }
 
-group = "org.jetbrains.research.ml.ast.transformations"
-version = "1.0-SNAPSHOT"
-
-repositories {
-    mavenCentral()
-    jcenter()
-}
-
-dependencies {
-    implementation(kotlin("stdlib-jdk8"))
-
-    implementation("com.github.gumtreediff", "core", "2.1.2")
-    implementation("org.apache.commons:commons-lang3:3.12.0")
-
-    // Need for CLI
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-runtime:1.0-M1-1.4.0-rc-218")
-    implementation("com.charleskorn.kaml:kaml:0.27.0")
-    implementation("org.jetbrains.kotlin:kotlin-reflect:1.4.20")
-    implementation("org.reflections:reflections:0.9.12")
-    implementation("com.xenomachina:kotlin-argparser:2.0.7")
-}
-
-intellij {
-    type = "PC"
-    version = "2020.3.3"
-    downloadSources = false
-    setPlugins("PythonCore")
-    updateSinceUntilBuild = true
-}
-
-ktlint {
-    enableExperimentalRules.set(true)
-}
-
-tasks {
-    withType<JavaCompile> {
-        sourceCompatibility = "11"
-        targetCompatibility = "11"
+allprojects {
+    repositories {
+        mavenCentral()
+        jcenter()
     }
-    withType<KotlinCompile> {
-        kotlinOptions.jvmTarget = "11"
+    apply {
+        plugin("org.jlleitschuh.gradle.ktlint")
     }
 
-    runIde {
-        val input: String? by project
-        val output: String? by project
-        val yaml: String? by project
-        args = listOfNotNull(
-            "python-transformations",
-            input?.let { "--input_path=$it" },
-            output?.let { "--output_path=$it" },
-            yaml?.let { "--yaml_path=$it" }
-        )
-        jvmArgs = listOf("-Djava.awt.headless=true", "--add-exports", "java.base/jdk.internal.vm=ALL-UNNAMED")
-        standardInput = System.`in`
-        standardOutput = System.`out`
-    }
-
-    register("cli") {
-        dependsOn("runIde")
+    ktlint {
+        enableExperimentalRules.set(true)
     }
 }
 
-// According to this topic:
-// https://intellij-support.jetbrains.com/hc/en-us/community/posts/360010164960-Build-Intellij-plugin-in-IDEA-2019-1-2020-3?page=1#community_comment_360002517940
-tasks.withType<org.jetbrains.intellij.tasks.BuildSearchableOptionsTask>()
-    .forEach { it.enabled = false }
+subprojects {
+    group = "org.jetbrains.research.ml.ast.transformations"
+    version = "1.0-SNAPSHOT"
+    apply {
+        plugin("java")
+        plugin("kotlin")
+        plugin("org.jetbrains.intellij")
+        plugin("com.github.johnrengelman.shadow")
+        plugin("org.jetbrains.dokka")
+        plugin("org.jetbrains.kotlin.plugin.serialization")
+    }
+
+    intellij {
+        type = "PC"
+        version = "2020.3.3"
+        downloadSources = false
+        setPlugins("PythonCore")
+        updateSinceUntilBuild = true
+    }
+    tasks {
+        withType<JavaCompile> {
+            sourceCompatibility = "11"
+            targetCompatibility = "11"
+        }
+        withType<KotlinCompile> {
+            kotlinOptions.jvmTarget = "11"
+        }
+        // According to this topic:
+        // https://intellij-support.jetbrains.com/hc/en-us/community/posts/360010164960-Build-Intellij-plugin-in-IDEA-2019-1-2020-3?page=1#community_comment_360002517940
+        withType<org.jetbrains.intellij.tasks.BuildSearchableOptionsTask>()
+            .forEach { it.enabled = false }
+    }
+
+    dependencies {
+        implementation(kotlin("stdlib-jdk8"))
+        implementation("com.github.gumtreediff", "core", "2.1.2")
+        implementation("org.apache.commons:commons-lang3:3.12.0")
+    }
+}
+
+//
+// plugins {
+//    java
+//    kotlin("jvm") version "1.4.30"
+//    id("org.jetbrains.intellij") version "0.7.2"
+//    id("com.github.johnrengelman.shadow") version "5.1.0"
+//    id("org.jetbrains.dokka") version "0.10.1"
+//    id("org.jlleitschuh.gradle.ktlint") version "9.4.1"
+//    kotlin("plugin.serialization") version "1.4.20"
+// }
+//
+//
+// repositories {
+//    mavenCentral()
+//    jcenter()
+// }
+//
+// dependencies {
+//    implementation(kotlin("stdlib-jdk8"))
+//
+//    implementation("com.github.gumtreediff", "core", "2.1.2")
+//    implementation("org.apache.commons:commons-lang3:3.12.0")
+//
+//    // Need for CLI
+//    implementation("org.jetbrains.kotlinx:kotlinx-serialization-runtime:1.0-M1-1.4.0-rc-218")
+//    implementation("com.charleskorn.kaml:kaml:0.27.0")
+//    implementation("org.jetbrains.kotlin:kotlin-reflect:1.4.20")
+//    implementation("org.reflections:reflections:0.9.12")
+//    implementation("com.xenomachina:kotlin-argparser:2.0.7")
+// }
+//
+// intellij {
+//    type = "PC"
+//    version = "2020.3.3"
+//    downloadSources = false
+//    setPlugins("PythonCore")
+//    updateSinceUntilBuild = true
+// }
+//
+// ktlint {
+//    enableExperimentalRules.set(true)
+// }
+//
+// tasks {
+//    withType<JavaCompile> {
+//        sourceCompatibility = "11"
+//        targetCompatibility = "11"
+//    }
+//    withType<KotlinCompile> {
+//        kotlinOptions.jvmTarget = "11"
+//    }
+//
+//    runIde {
+//        val input: String? by project
+//        val output: String? by project
+//        val yaml: String? by project
+//        args = listOfNotNull(
+//            "python-transformations",
+//            input?.let { "--input_path=$it" },
+//            output?.let { "--output_path=$it" },
+//            yaml?.let { "--yaml_path=$it" }
+//        )
+//        jvmArgs = listOf("-Djava.awt.headless=true", "--add-exports", "java.base/jdk.internal.vm=ALL-UNNAMED")
+//        standardInput = System.`in`
+//        standardOutput = System.`out`
+//    }
+//
+//    register("cli") {
+//        dependsOn("runIde")
+//    }
+// }
+//
---
+++ settings.gradle.kts
@@ -0,0 +1,6 @@
+rootProject.name = "ast-transformations"
+
+include(
+    "ast-transformations-core",
+    "ast-transformations-plugin"
+)
