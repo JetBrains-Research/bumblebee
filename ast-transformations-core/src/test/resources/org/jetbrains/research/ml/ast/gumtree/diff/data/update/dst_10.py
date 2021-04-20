293d5af9f0c74679799c5dbb56dd3fbf9cb1cad0
Artem Bobrov <artembobrov3059@gmail.com>
2021-04-20 09:45:03 +0300

    add test

---
+++ ast-transformations-core/src/test/resources/org/jetbrains/research/ml/ast/gumtree/diff/data/update/dst_10.py
@@ -0,0 +1,8 @@
+v1 = 0
+v2 = 0
+v3 = [int(v4) for v4 in input().split()]
+for v5 in v3:
+    if v5 == 0:
+        v1 = 1 + v1
+    else:
+        v2 = 1 + v2
---
+++ ast-transformations-core/src/test/resources/org/jetbrains/research/ml/ast/gumtree/diff/data/update/src_10_if_statement.py
@@ -0,0 +1,6 @@
+v1 = [int(v2) for v2 in input().split()]
+v3 = 0
+
+for v4 in v1:
+    if v4 == 0:
+        pass
